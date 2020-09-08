1e6c156dcfb8374bc9149ead99aa51e04476f256
Tóth Zsófia <zsofiath@gmail.com>
2020-09-08 17:32:58 +0200

    adding up button

--- app/src/main/java/com/tzs/myteaapplication/MainActivity.kt
+++ app/src/main/java/com/tzs/myteaapplication/MainActivity.kt
@@ -2,10 +2,20 @@ package com.tzs.myteaapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return navController.navigateUp()
     }
 }
