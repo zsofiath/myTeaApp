18c398c81707e11064a5198359af64f402f334b5
Tóth Zsófia <zsofiath@gmail.com>
2020-10-19 11:30:06 +0200

    refactor adapter

--- app/src/main/java/com/tzs/myteaapplication/Adapters/TeaListItemAdapter.kt
+++ app/src/main/java/com/tzs/myteaapplication/Adapters/TeaListItemAdapter.kt
@@ -1,15 +1,12 @@
 package com.tzs.myteaapplication.Adapters
 
-import android.graphics.Color
+import android.content.res.Resources
 import android.view.LayoutInflater
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.TextView
-import androidx.appcompat.view.menu.ActionMenuItemView
 import com.tzs.myteaapplication.R
-import com.tzs.myteaapplication.TeaItemViewHolder
 import com.tzs.myteaapplication.database.Tea
 
 class TeaListItemAdapter: RecyclerView.Adapter<TeaListItemAdapter.ViewHolder>() {
@@ -23,22 +20,37 @@ class TeaListItemAdapter: RecyclerView.Adapter<TeaListItemAdapter.ViewHolder>() 
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = data[position]
-        // if(item black tea) kell else ág
-       // holder.textView.setTextColor(Color.CYAN)
-        holder.btn.text = item.name
-        //holder.btn.setBackgroundColor(6)
+        holder.bind(item)
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        val view = layoutInflater.inflate(R.layout.tea_button, parent, false)
-
-        return ViewHolder(view)
+        return ViewHolder.from(parent)
     }
 
-    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+    class ViewHolder private constructor(itemView: View): RecyclerView.ViewHolder(itemView){
         val btn: Button = itemView.findViewById(R.id.tea_list_button)
+        val res = itemView.context.resources
+        fun bind(
+            item: Tea
+        ) {
+            var z = res
+            // if(item black tea) kell else ág
+            // holder.textView.setTextColor(Color.CYAN)
+            btn.text = item.name
+            //holder.btn.setBackgroundColor(6)
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): ViewHolder {
+                val view = LayoutInflater.from(parent.context).inflate(R.layout.tea_button, parent, false)
+
+                return ViewHolder(view)
+            }
+        }
     }
+
+
 }
 
 
