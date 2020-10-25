045b99338eb0b41d96f9e0d7a29a2b2daa5364c9
Tóth Zsófia <zsofiath@gmail.com>
2020-10-25 21:15:46 +0100

    write test

---
+++ app/src/test/java/com/tzs/myteaapplication/viewmodel/TeaViewModelTest.kt
@@ -0,0 +1,30 @@
+package com.tzs.myteaapplication.viewmodel
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.tzs.myteaapplication.models.Tea
+import com.tzs.myteaapplication.models.TeaTypes
+import org.junit.Assert
+import org.junit.Assert.*
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class TeaViewModelTest {
+    @Test
+    fun setViewedTea(){
+        var t = Tea(1)
+        t.name = "name"
+        t.type = TeaTypes.GREEN
+        t.temp=100
+        t.amount=5
+
+        Tea.currentTea = t
+
+        var vm = TeaViewModel(1)
+
+
+        Assert.assertEquals(t.amount.toString() + "g/100ml", vm.currentTea_AmountOfLeaf)
+        Assert.assertEquals(t.temp.toString() + "C°", vm.currentTea_BrewingTemperature)
+
+    }
+}
