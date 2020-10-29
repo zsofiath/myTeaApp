cb45d3ebae51584e79df71b4a78fa761f6a0b69c
Tóth Zsófia <zsofiath@gmail.com>
2020-10-29 17:25:25 +0100

    Infusion works

--- app/src/main/java/com/tzs/myteaapplication/models/Infusion.kt
+++ app/src/main/java/com/tzs/myteaapplication/models/Infusion.kt
@@ -1,5 +1,9 @@
 package com.tzs.myteaapplication.models
 
+import kotlinx.coroutines.delay
+import kotlin.concurrent.thread
+
+
 class Infusion {
     var value: Int = 0
 
@@ -8,4 +12,14 @@ class Infusion {
         set(inValue) {
             value = inValue.toInt()
         }
+
+    fun countDown(){
+        thread {
+            while (value > 0) {
+                Thread.sleep(1000)
+                value--
+            }
+
+        }
+    }
 }
--- app/src/test/java/com/tzs/myteaapplication/models/InfusionTest.kt
+++ app/src/test/java/com/tzs/myteaapplication/models/InfusionTest.kt
@@ -1,6 +1,7 @@
 package com.tzs.myteaapplication.models
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.apache.tools.ant.taskdefs.WaitFor.ONE_SECOND
 import org.junit.Assert
 import org.junit.Assert.*
 import org.junit.Test
@@ -43,6 +44,21 @@ class InfusionTest {
         inf.visibleValue = "100"
 
         Assert.assertEquals(100, inf.value)
+    }
+
+    @Test
+    fun countDown(){
+        var inf = Infusion()
+        inf.visibleValue = "3"
+
+        inf.countDown()
+        Assert.assertEquals(3, inf.value)
+
+        Thread.sleep(ONE_SECOND*1+10);
+        Assert.assertEquals(2, inf.value)
+
+        Thread.sleep(ONE_SECOND*3);
+        Assert.assertEquals(0, inf.value)
 
     }
 }
